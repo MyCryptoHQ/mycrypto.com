42fd366f228e4e6533890ae12f7b9339705f8559
tayvano <tayvano@gmail.com>
2018-04-10 00:10:10 -0700

    update in footer (not sure if does anything?)

--- app/scripts/controllers/footerCtrl.js
+++ app/scripts/controllers/footerCtrl.js
@@ -22,11 +22,11 @@ var footerCtrl = function($scope, globalService) {
     }
     var setGasValues = function() {
         $scope.gas = {
-            curVal: 41,
-            value: globalFuncs.localStorage.getItem(gasPriceKey, null) ? parseInt(globalFuncs.localStorage.getItem(gasPriceKey)) : 41,
+            curVal: 10,
+            value: globalFuncs.localStorage.getItem(gasPriceKey, null) ? parseInt(globalFuncs.localStorage.getItem(gasPriceKey)) : 10,
             max: 99,
-            min: 1,
-            step: 1
+            min: 0.1,
+            step: 0.1
         }
         ethFuncs.gasAdjustment = $scope.gas.value;
     }
