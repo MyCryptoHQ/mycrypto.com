9b7a6a65a12a3256fe4874239d27e98b873dd856
Taylor Monahan <7924827+tayvano@users.noreply.github.com>
2018-04-10 00:07:35 -0700

    Reduce minimum gas price to 0.1 GWEI

--- app/scripts/controllers/tabsCtrl.js
+++ app/scripts/controllers/tabsCtrl.js
@@ -42,21 +42,21 @@ var tabsCtrl = function($scope, globalService, $translate, $sce) {
     $scope.gasChanged = function() {
         globalFuncs.localStorage.setItem(gasPriceKey, $scope.gas.value);
         ethFuncs.gasAdjustment = $scope.gas.value;
-        $scope.gasPriceMsg = ethFuncs.gasAdjustment < 41 ? true : false
+        $scope.gasPriceMsg = ethFuncs.gasAdjustment < 10 ? true : false
     }
     var setGasValues = function() {
         $scope.gas = {
-            curVal: 41,
-            value: globalFuncs.localStorage.getItem(gasPriceKey, null) ? parseInt(globalFuncs.localStorage.getItem(gasPriceKey)) : 41,
+            curVal: 10,
+            value: globalFuncs.localStorage.getItem(gasPriceKey, null) ? parseInt(globalFuncs.localStorage.getItem(gasPriceKey)) : 10,
             max: 99,
-            min: 1,
-            step: 1
+            min: .1,
+            step: .1
         }
 
         var curNode = globalFuncs.localStorage.getItem('curNode', null);
 
         ethFuncs.gasAdjustment = $scope.gas.value;
-        $scope.gasPriceMsg = ethFuncs.gasAdjustment < 41 ? true : false
+        $scope.gasPriceMsg = ethFuncs.gasAdjustment < 10 ? true : false
     }
     setGasValues();
     $scope.gasChanged();
